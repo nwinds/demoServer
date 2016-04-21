db8ed11e4cfa5b9aa39ced78d19ab461d61df1df
nwinds <namingwinds@gmail.com>
2016-04-21 09:20:26 +0000

    add web ui for post

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ python tornado for server
 
 
 # vs Rails
+
+
+F&A
+-----------------
+
+### port in use
+$ /etc/init.d/networking restart
+> [ref](http://serverfault.com/questions/329845/how-to-forcibly-close-a-socket-in-time-wait)
+
+
--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+import textwrap
 import tornado.httpserver
+
 import tornado.ioloop
 import tornado.options
 import tornado.web
-
+# import tornado.wsgi
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
@@ -10,10 +12,31 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         greeting = self.get_argument('greeting', 'Hello')
         self.write(greeting + ', friendly user!')
+    # def post(self, widget_id):
+        # widget = retrieve_from_db(widget_id)
+        # widget['foo'] = self.get_argument('foo')
+        # save_to_db(widget)
+
+
+class ReverseHandler(tornado.web.RequestHandler):
+    def get(self, input):
+        self.write(input[::-1])
+
+class WrapHandler(tornado.web.RequestHandler):
+    def post(self):
+        text = self.get_argument('text')
+        width = self.get_argument('width', 40)
+        self.write(textwrap.fill(text, int(width)))
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", MainHandler)])
+    app = tornado.web.Application(
+        handlers=[(r"/", MainHandler),
+                  (r"/reverse/(\w+)", ReverseHandler),
+                  (r"/wrap", WrapHandler),
+                  (r"/gateway", tornado.web.RedirectHandler,
+                   dict(url="https://gateway/index.php")),
+                  ])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
