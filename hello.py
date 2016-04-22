6b4e1d00025828cd6b71c503ffd1a4efd033cd09
nwinds <namingwinds@gmail.com>
2016-04-22 05:19:38 +0000

    add login authentication

--- hello.py
+++ hello.py
@@ -10,15 +10,39 @@ import os
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
-class MainHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
+        # tornado.web.authenticated did the following work
         # secure cookie - ver 1.0
-        if not self.get_secure_cookie("mycookie"):
-            self.set_secure_cookie("mycookie", "myvalue")
-            self.write("Your cookie was not set yet!")
-        else:
-            self.write("Your cookie was set!")
+        # if not self.current_user:
+            # self.redirect("/login")
+            # return
+
+        # if not self.get_secure_cookie("mycookie"):
+            # self.set_secure_cookie("mycookie", "myvalue")
+            # self.write("Your cookie was not set yet!")
+        # else:
+        name  = tornado.escape.xhtml_escape(self.current_user)
+        self.write("Hello, " + name)
+        items = ["Item 1", "Item 2", "Item 3"]
+        self.render("template.html", title="My title", items=items)
         self.render('index.html')
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.write('<html><body><form action="login" method="post">'
+                   'Name: <input type="text" name="name">'
+                   ' <input type="submit" value="Sign in">'
+                   ' </form></body></html>')
+
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
 
 class FormPageHandler(tornado.web.RequestHandler):
     def post(self):
@@ -32,14 +56,22 @@ class FormPageHandler(tornado.web.RequestHandler):
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
-    # wsgi.WSGIApplication
+
+    settings = {
+            "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            "login_url": "/login",
+
+    }
+
     app = tornado.wsgi.WSGIApplication(
         handlers=[(r'/', MainHandler),
+                  (r'login', LoginHandler),
                   (r'/poem', FormPageHandler),
                   (r"/gateway", tornado.web.RedirectHandler,
                     dict(url="https://gateway/index.php")),
-                  ], cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-        template_path=os.path.join(os.path.dirname(__file__), "templates")
+                  ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        **settings
     )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
